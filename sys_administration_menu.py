c389e7b67279996d0e0282f771068288bf1c92ce
masaadotaibi <masaadbal@gmail.com>
2023-03-02 18:52:19 +0300

    Initial commit
    Python program for enhancing and easing the experience of linux
    administrations tasks
    
    @mosaed-otaibi

---
+++ sys_administration_menu.py
@@ -0,0 +1,30 @@
+import os
+
+
+def add_user():
+    pass
+
+def remove_user():
+    pass
+
+def add_group():
+    pass
+
+def remove_group():
+    pass
+
+
+def show_group():
+    pass
+
+
+def show_grp_and_users():
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
