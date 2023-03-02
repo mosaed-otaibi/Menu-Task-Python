ea6e85da66f894e9a8a232b6df8694c033c098ad
masaadotaibi <masaadbal@gmail.com>
2023-03-02 19:54:15 +0300

    Added the menu list and adding & deleting user services
    Commands list menu function added, and the basic functions of adding
    and deleting a user is added. Also, basic checking function is added,
    that ask_for_continuing() which asks the user if they wish to continue
    using the program services.
    
    @mosaed-otaibi

--- sys_administration_menu.py
+++ sys_administration_menu.py
@@ -1,11 +1,29 @@
 import os
-
+import subprocess
 
 def add_user():
-    pass
+    print("Wlcome to adding user service!")
+
+    confirm = "N"
+
+    while confirm != "Y":
+        username = input("Enter the name of the user: ")
+        print("Do you want to add the user '" + username + "'? (Y/N)", end="")
+        confirm = input().upper()
+    os.system("sudo adduser " + username)
+
 
 def remove_user():
-    pass
+    print("Wlcome to deleting user service!")
+
+    confirm = "N"
+
+    while confirm != "Y":
+        username = input("Enter the name of the user: ")
+        print("Do you want to add the user '" + username + "'? (Y/N)", end="")
+        confirm = input().upper()
+    subprocess.run(["sudo", "userdel", username])
+
 
 def add_group():
     pass
@@ -14,16 +32,59 @@ def remove_group():
     pass
 
 
-def show_group():
-    pass
-
-
 def show_grp_and_users():
     pass
 
 
+
+def list_administration_options():
+    print("""
+        \n\nBelow listed the available commands. Enter one of the following(or 'q' to exit):
+        \n\t - '1' to Add a user
+        \n\t - '2' to remove a user
+        \n\t - '3' to add a group
+        \n\t - '4' to remove a group
+        \n\t - '5' to show a group and its users
+    """)
+
+
+def ask_for_continuing():
+    continueCheck = input("Do you want to use other commands? (Y/n)")
+    return continueCheck.upper()
+
+
 def main():
-    pass
+
+    print("Welcome to our program of linux administration!")
+
+    while True:
+        list_administration_options()
+
+        choice = input("Choose from the list of commands above: ")
+
+        if choice == "q":
+            exitCheck = input("Do you want to exit (enter 'y' to exit)? ")
+            if exitCheck == "y":
+                break
+        elif choice == "1":
+            add_user()
+            if ask_for_continuing() == "N":
+                break
+        elif choice == "2":
+            remove_user()
+            if ask_for_continuing() == "N":
+                break
+        elif choice == "3":
+            add_group()
+        elif choice == "4":
+            remove_group()
+        elif choice == "5":
+            show_grp_and_users()
+        else:
+            print("Wrong choice! Please select one of the availble commands listed below!")
+            continue
+
+
 
 if __name__ == "__main__":
     main()
